f075e7df3ce9371b89f7def7216e283c22be8e6b
Violet Kester <kestervj89@gmail.com>
2023-09-16 13:56:13 -0400

    set up admin page

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Employee
+
+@admin.register(Employee)
+class PostAdmin(admin.ModelAdmin):
+    # set columns to be displayed
+    list_display = ['first_name', 'last_name']
+
+    # set column filters
+    list_filter = ['first_name', 'last_name']
+
+    # # set search fields
+    search_fields = ['first_name', 'last_name']
+
+    # order columns by status and publish by default
+    ordering = ['last_name', 'first_name']
