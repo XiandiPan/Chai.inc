1f7150362df5171bad8dc89f81ba2fe0713b8d08
Violet Kester <kestervj89@gmail.com>
2023-09-16 14:47:09 -0400

    editing employee model

--- schedule/models.py
+++ schedule/models.py
@@ -1,7 +1,16 @@
 from django.db import models
+# from datetime import timezone
 
-# Create your models here.
 class Employee(models.Model):
+
+    # class Shifts(models.TextChoices):
+    #     MONDAY_AM = 'MO-AM', 'Monday AM'
+    #     MONDAY_PM = 'MO-PM', 'Monday PM'
+    #     TUESDAY_AM = 'TU-AM', 'Tuesday AM'
+    #     TUESDAY_PM = 'TU-PM', 'Tuesday PM'
 
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    # availability = models.CharField(max_length=5,
+    #                           choices=Shifts.choices,
+    #                           default=Shifts.MONDAY_AM)
